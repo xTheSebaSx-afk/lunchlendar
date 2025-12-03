fab91ef6b37fd5d3b41d98fbdea5f09d829b4558
xTheSebaSx-afk <xthesebasx123@gmail.com>
2025-12-03 10:00:09 -0500

    estilo: mejorar el encabezado de bienvenida en el panel de usuario

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -12,7 +12,7 @@ function Panel() {
 
     return (
         <>
-            <h1>Bienvenido {user.username}</h1>
+            <h1 className='w-screen p-3 '>Bienvenido <span className='text-emerald-500'>{user.username}</span></h1>
             <button className='absolute top-0 right-0 m-3 mr-4 rounded border-emerald-200 to-red-400 from-blue-300 bg-linear-210 p-3' onClick={handleLogout}>Logout</button>
         </>
     )
