d91e87793bd700e839a4d1b48b041f7e77349e74
xTheSebaSx-afk <xthesebasx123@gmail.com>
2025-12-09 19:11:21 -0500

    fix: corregir la URL de la API para manejar entornos de producción y desarrollo

--- client/src/api/ApiManager.jsx
+++ client/src/api/ApiManager.jsx
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_ENVIRONMENT == "production" ? import.meta.env.VITE_API_URL : import.meta.env.VITE_API_UR_BASE;
 
-// Crea una instancia global para todas las peticiones
 const api = axios.create({
-    baseURL: API_URL,    // NO poner "/" extra si VITE_API_URL ya lo trae
+    baseURL: API_URL,
     withCredentials: true,
 });
 
-// Importante: permitir cookies en CORS preflight
 api.defaults.withCredentials = true;
 
 export default api;
