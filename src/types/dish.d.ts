c69e4be79704c5b06d7ecf8446901eb1895016b0
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-01-23 22:21:50 -0500

    feat: agregar campos de comentarios, likes y dislikes a la interfaz Dish

--- src/types/dish.d.ts
+++ src/types/dish.d.ts
@@ -1,9 +1,18 @@
 export interface Dish {
     _id: string;
     author: number;
+    comments: Comment[];
     description: string;
+    dislikes: number;
     ingredients: string[];
+    likes: number;
     name: string;
     price: number;
 }
 
+export interface Comment {
+    _id: string;
+    author: number;
+    date: Date;
+    text: string;
+}
