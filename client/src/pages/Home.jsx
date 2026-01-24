28f7bf505e24a649ee9131b507979a9564cbf1e7
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-01-23 22:22:20 -0500

    feat: integrar autenticación de usuario en la barra de navegación y encabezado

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
+import { useUser } from '../context/UserContext'
 
-const NavBar = () => {
+const NavBar = ({ isAuthenticated, user }) => {
     return (
         <nav className="p-2 to-[#c1dff9] from-[#e8f2fc] bg-linear-to-l flex justify-around items-center relative top-3 [box-shadow:0px_1px_10px_2px_#abc6dd] text-[#1b3a49] text-[18px] text-center">
             <ul className='md:w-100 flex w-50 justify-around'>
@@ -8,22 +9,26 @@ const NavBar = () => {
                 <li className='cursor-pointer hover:bg-[#a5b1bd62] rounded 2xl px-5 py-2 transition-all duration-300'>Item 2</li>
                 <li className='cursor-pointer hover:bg-[#a5b1bd62] rounded 2xl px-5 py-2 transition-all duration-300'>Item 3</li>
             </ul>
-            <Link to="/login" target='_blank' className='from-[#94c255] to-[#398a5b] from-30% bg-linear-150 px-5 py-2 rounded border-[#366f43] text-white transition-all duration-300 hover:px-7 hover:py-4 hover:text-2xl'>Login</Link>
+            {!isAuthenticated ? (
+                <Link className="from-[#94c255] to-[#398a5b] from-30% bg-linear-150 px-5 py-2 rounded border-[#366f43] text-white transition-all duration-300 hover:scale-105 brightness-110" to="/login" target="_blank">Login</Link>
+            ) : (<>
+                <p>Bienvenido, <span className='text-shadow-[0_0px_2px_#000] underline'>{user.username}</span></p>
+            </>)}
         </nav>
     )
 }
 
 
-const Header = ({ children }) => {
+const Header = ({ isAuthenticated, user }) => {
     return (
         <>
             <header className='header py-10 md:h-[570px] h-[500px]' >
-                <NavBar />
+                <NavBar isAuthenticated={isAuthenticated} user={user} />
                 <div className='flex justify-center'>
                     <div className='text-center mt-[120px]'>
                         <h1 className='md:text-7xl sm:text-5xl text-3xl text-[#23282c]'>Lunch Calendar</h1>
                         <p className='text-center mt-2'>Una aplicación web para ayudarte a planear tus comidas</p>
-                        <button className='p-4 text-white mt-6 text-[20px] w-[150px] from-[#48945c] to-[#88bf4a] bg-linear-60 rounded-2xl cursor-pointer focus:outline-[#46FF17]'>Comenzar</button>
+                        <button className='mt-10'><Link to="/dishes" className='p-4 text-white text-[20px] w-[150px] from-[#48945c] to-[#88bf4a] bg-linear-60 rounded-2xl focus:outline-[#46FF17]'>Comenzar</Link></button>
                     </div>
                 </div>
             </header >
@@ -37,7 +42,7 @@ const ObjectiveItem = ({ icon, title, description }) => {
 
             {/* Círculo del icono */}
             <div className="md:w-[150px] md:h-[150px] sm:size-[120px] size-[60px] rounded-full bg-white shadow-md flex items-center justify-center shrink-0" >
-                <img src={icon} alt={title} className="md:w-[100px] md:h-[100px] sm:size-[100px] size-[40px] object-contain" />
+                <img src={icon} alt={title} className="md:w-[100px] md:h-[100px] sm:size-[100px] size-10 object-contain" />
             </div>
 
             {/* Texto */}
@@ -109,10 +114,12 @@ const Footer = () => {
 }
 
 function Home() {
+    const { user, isAuthenticated } = useUser()
+
     return (
         <div className=''>
             <img src="/icon.svg" alt="lunchcalendar" className='size-[100px] bg-linear-120 from-[#8cbd56] to-[#b8defc] rounded-full z-10 bottom-0 right-0 m-4 fixed' />
-            <Header />
+            <Header isAuthenticated={isAuthenticated} user={user} />
             <Main />
             <Footer />
         </div>
