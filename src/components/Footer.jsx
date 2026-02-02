ba8bc32f0f0018794c72ecd3a5d722a79cfcbdce
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-02-02 11:34:43 -0500

    feat: agregar componente LoginButton y actualizar Footer y Home para su integración

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,60 @@
 import { Link } from 'react-router-dom'
 
+const Links = () => {
+    return (
+        <ul className='w-[screen] flex justify-around'>
+            <li className='font-bold text-3xl underline'>
+                Lunch Calendar
+            </li>
+            <li>
+                <ul>
+                    <li>Quick Links</li>
+                    <li>asd</li>
+                    <li>asd</li>
+                    <li>asd</li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li>Legal</li>
+                    <li>asd</li>
+                    <li>asd</li>
+                    <li>asd</li>
+                </ul>
+            </li>
+        </ul>
+    )
+}
+
+const SocialLinks = () => {
+    return (
+        <ul className='flex flex-col gap-4 justify-between px-4 mt-8'>
+            <li>
+                <ul className='flex gap-5 items-baseline'>
+                    <li>
+                        <Link to="https://github.com/xTheSebaSx-afk" className='hover:underline text-[#1905b4]' target='_blank'>
+                            <img src="/images/illustrations/xthesebasx.png" alt="" className='transition-all duration-300 hover:scale-125 rounded-full size-[45px]' />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="https://github.com/xTheSebaSx-afk/lunchlendar" target='_blank'>
+                            <img src="/icons/github.svg" alt="" className='transition-all duration-300 hover:scale-125 rounded-full size-[45px]' />
+                        </Link>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                © {new Date().getFullYear()} Lunch Calendar. All rights reserved.
+            </li>
+        </ul>
+    )
+}
+
 function Footer() {
     return (
-        <footer className='shadow [box-shadow:0px_1px_10px_2px_#abc6dd] py-10 bg-gray-400/30 mt-20'>
-            <ul className='w-[screen] flex justify-around'>
-                <li className='font-bold text-3xl underline'>
-                    Lunch Calendar
-                </li>
-                <li>
-                    <ul>
-                        <li>Quick Links</li>
-                        <li>asd</li>
-                        <li>asd</li>
-                        <li>asd</li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <li>Legal</li>
-                        <li>asd</li>
-                        <li>asd</li>
-                        <li>asd</li>
-                    </ul>
-                </li>
-            </ul>
-            <ul className='flex mx-auto gap-4 justify-around mt-4'>
-                <li>
-                    © {new Date().getFullYear()} Lunch Calendar. All rights reserved.
-                </li>
-                <li>
-                    Developed by <Link to="https://github.com/xTheSebaSx-afk" className='hover:underline text-[#1905b4]' target='_blank'>xBast1q</Link>.
-                </li>
-                <li>
-                    Github Repo: <Link to="https://github.com/xTheSebaSx-afk/lunchlendar" className='hover:underline text-[#1905b4]' target='_blank'>Lunch Calendar</Link>
-                </li>
-            </ul>
+        <footer className='shadow [box-shadow:0px_1px_10px_2px_#abc6dd] py-10 bg-gray-400/30 mt-20 footer'>
+            <Links />
+            <SocialLinks />
         </footer>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useUser } from '../context/UserContext'
 import Footer from '../components/Footer'
+import { useState } from 'react';
+import LoginButton from '../components/LoginButton';
 
-const NavBar = ({ isAuthenticated, user }) => {
+const NavBar = () => {
+
     return (
         <nav className="p-2 to-[#c1dff9] from-[#e8f2fc] bg-linear-to-l flex justify-around items-center relative top-3 [box-shadow:0px_1px_10px_2px_#abc6dd] text-[#1b3a49] text-[18px] text-center">
             <ul className='md:w-100 flex w-50 justify-around'>
@@ -10,21 +13,17 @@ const NavBar = ({ isAuthenticated, user }) => {
                 <li className='cursor-pointer hover:bg-[#a5b1bd62] rounded 2xl px-5 py-2 transition-all duration-300'>Item 2</li>
                 <li className='cursor-pointer hover:bg-[#a5b1bd62] rounded 2xl px-5 py-2 transition-all duration-300'>Item 3</li>
             </ul>
-            {!isAuthenticated ? (
-                <Link className="from-[#94c255] to-[#398a5b] from-30% bg-linear-150 px-5 py-2 rounded border-[#366f43] text-white transition-all duration-300 hover:scale-105 brightness-110" to="/login" target="_blank">Login</Link>
-            ) : (<>
-                <p>Bienvenido, <span className='text-shadow-[0_0px_2px_#000] underline'>{user.username}</span></p>
-            </>)}
+            <LoginButton />
         </nav>
     )
 }
 
 
-const Header = ({ isAuthenticated, user }) => {
+const Header = ({  }) => {
     return (
         <>
             <header className='header py-10 md:h-[570px] h-[500px]' >
-                <NavBar isAuthenticated={isAuthenticated} user={user} />
+                <NavBar />
                 <div className='flex justify-center'>
                     <div className='text-center mt-[120px]'>
                         <h1 className='md:text-7xl sm:text-5xl text-3xl text-[#23282c]'>Lunch Calendar</h1>
@@ -79,12 +78,16 @@ const Main = () => {
 }
 
 function Home() {
-    const { user, isAuthenticated } = useUser()
+    const { user, isAuthenticated, logout } = useUser()
+
+    const handleLogout = async () => {
+        await logout();
+    }
 
     return (
         <div className=''>
             <img src="/images/ui/icon.svg" alt="lunchcalendar" className='size-[100px] bg-linear-120 from-[#8cbd56] to-[#b8defc] rounded-full z-10 bottom-0 right-0 m-4 fixed' />
-            <Header isAuthenticated={isAuthenticated} user={user} />
+            <Header />
             <Main />
             <Footer />
         </div>
