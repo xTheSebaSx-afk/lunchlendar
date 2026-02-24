994c38b0eccc3538f2ae949790db3c1561d444db
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-02-24 09:07:31 -0500

    Agregado alias para components e íconos

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@icons": path.resolve(__dirname, "src/components/icons"),
+      "@components": path.resolve(__dirname, "src/components"),
+    }
+  }
 })
