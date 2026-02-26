65a4a2f6d2a907c9305dad287b7284bd405ee67e
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-02-26 15:51:22 -0500

    feat: agregar nuevos íconos y actualizar íconos existentes

---
+++ src/components/icons/DropDownIcon.jsx
@@ -0,0 +1,7 @@
+const DropDownIcon = (props) => (
+	<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+		<path fill={props.fill || "currentColor"} d="m7 10l5 5 5-5z" />
+	</svg>
+)
+
+export default DropDownIcon
---
+++ src/components/icons/DropUpIcon.jsx
@@ -0,0 +1,7 @@
+const DropUpIcon = (props) => (
+	<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+		<path fill="currentColor" d="m7 14l5-5l5 5z" ></path>
+	</svg>
+)
+
+export default DropUpIcon
