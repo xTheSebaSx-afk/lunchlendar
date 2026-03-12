64dd0a16c003565f2753393e5cd76220ff5afb99
xTheSebaSx-afk <xthesebasx123@gmail.com>
2026-03-12 13:11:28 -0500

    feat: mejorar diseño de recetas destacadas y agregar nuevos íconos para interacciones

--- client/src/pages/Dishes.jsx
+++ client/src/pages/Dishes.jsx
@@ -1,4 +1,3 @@
-import { useUser } from '../context/UserContext'
 import { useEffect, useState } from 'react'
 import { useDishes } from '../context/DishesContext'
 import { Link, useSearchParams } from 'react-router-dom'
@@ -7,8 +6,9 @@ import LoginButton from '@components/LoginButton'
 import DislikeIcon from '@components/icons/DislikeIcon'
 import LikeIcon from '@components/icons/LikeIcon'
 import SearchIcon from '@icons/SearchIcon'
-import LeaveIcon from '@components/icons/LeaveIcon'
+import ClockIcon from '@components/icons/ClockIcon'
 import AppIcon from '@components/icons/AppIcon'
+import CalendarIcon from '@components/icons/CalendarIcon'
 
 /**
  * 
@@ -18,41 +18,31 @@ const StarredRecipe = ({ dish }) => {
 
     if (!dish) return null;
 
-    console.log()
-
     return (
         <>
-            <div className="grid gridl-cols-[100%] md:grid-cols-[350px_1fr] grid-rows-[210px] overflow-hidden rounded-lg bg-[#fefcfe] p-2 my-4">
-                <img src="/images/illustrations/food.png" alt="" className='h-[210px] w-full md:w-[350px] rounded-l-lg' />
-                <div className="flex">
-                    <div className='w-[170%] bg-[#ffffff] px-3 flex-col flex'>
-                        <h3>{dish.name}</h3>
-                        <p>{dish.description}</p>
-                        <p>Precio: S/.<span>{dish.price.toFixed(2)}</span></p>
-                        <p>Categoría: {dish.category}</p>
-                        <div className='self-end mt-auto flex gap-4'>
-                            <div className='flex items-center gap-1'>
-                                <DislikeIcon className='size-[17px]' />
-                                <p>{dish.dislikes}</p>
-                            </div>
-                            <div className='flex items-center gap-1'>
-                                <LikeIcon className='size-[17px]' />
-                                <p>{dish.likes}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex justify-start capitalize flex-col bg-[#eaebea] w-full p-3'>
-                        <h3>Ingredientes</h3>
-                        <ul className='w-full flex flex-col justify-center gap-2 h-full'>
-                            {dish.ingredients.slice(0, 5).map(ingredient => (
-                                <li className='' key={ingredient}>
-                                    <p className='flex items-center gap-1'>
-                                        <LeaveIcon className='size-5' /> {ingredient}
-                                    </p>
-                                </li>
-                            ))}
+            <div className="rounded-lg bg-[#fefcfe] my-4">
+                <div className='rounded-t-lg text-center text-white text-shadow-lg shadow-[0_0px_-10px_#000] bg-[#4a9cbb] py-3'>
+                    <h2 className='text-3xl font-bold'>Receta destacada:</h2>
+                </div>
+                <div className='bg-cover bg-center h-80 w-full bg-[url("/images/illustrations/food.png")] rounded-b-lg shadow-xl relative'>
+                    <div className='absolute left-0 bottom-0 p-4 bg-[#bbfdfa88] rounded-tl-lg rounded-tr-lg w-full'>
+                        <h2 className='text-3xl font-bold text-white text-shadow-lg'>{dish.name}</h2>
+                        <p className='text-xl text-[#216776] font-bold'>{dish.description}</p>
+                        <ul className='flex gap-2 pt-2'>
+                            <li className='flex gap-1 items-center border-r border-[#938885] pr-2'>
+                                <LikeIcon className="size-7" fill="#4597a8" /> {dish.opinions.filter(x => x.type == "like").length}
+                            </li>
+                            <li className='flex gap-1 items-center border-r border-[#938885] pr-2'>
+                                <DislikeIcon className="size-7" fill="#77b4d5" /> {dish.opinions.filter(x => x.type == "dislike").length}
+                            </li>
+                            <li className='flex gap-1 items-center'>
+                                <ClockIcon className="size-7" fill_face="#313d46" fill_hand="#313d46" /> {dish.steps.length} pasos
+                            </li>
                         </ul>
                     </div>
+                    <span className='absolute right-0 bottom-10 bg-[#dae0e6] pr-8 pl-4 text-xl p-2 rounded-l-full text-[#216776] font-bold'>
+                        S/. {dish.price.toFixed(2)}
+                    </span>
                 </div>
             </div>
         </>
@@ -65,22 +55,31 @@ const StarredRecipe = ({ dish }) => {
 const RecipeCard = ({ dish }) => {
     return (
         <>
-            <div className="grid grid-cols-[100%]  md:grid-rows-[180x] overflow-hidden rounded-lg bg-[#fefcfe] p-2 hover:shadow-lg">
-                <img src="/images/illustrations/food.png" alt="" className='h-[180px] w-full  rounded-l-lg' />
-                <div className="bg-[#efeef0] p-2 flex flex-col">
-                    <h3 className="text-2xl w-max hover:underline"><Link className='block' to={`/dishes/${dish._id}`}>{dish.name}</Link></h3>
-                    <p className="">{dish.description}</p>
-                    <p className='text-[#565561]'>Precio: S/.<span>{dish.price.toFixed(2)}</span></p>
-                    <div className='self-end mt-auto flex gap-4'>
-                        <div className='flex items-center gap-1'>
-                            <DislikeIcon className='size-[17px]' />
-                            <p>{dish.dislikes}</p>
-                        </div>
-                        <div className='flex items-center gap-1'>
-                            <LikeIcon className='size-[17px]' />
-                            <p>{dish.likes}</p>
-                        </div>
+            <div className="grid md:grid-cols-[2fr_5fr] overflow-hidden rounded-lg hover:shadow-lg">
+                <img src="/images/illustrations/food.png" alt="" className='h-[157px] w-full  rounded-l-lg' />
+                <div className="bg-[#a86ef369] p-2 pl-4 flex flex-col relative text-wrap flex-wrap">
+                    <h3 className="text-[15px] font-bold md:text-2xl w-max hover:underline my-2"><Link className='block' to={`/dishes/${dish._id}`}>{dish.name}</Link></h3>
+                    <p className="text-[10px] md:text-[17px]">{dish.description}</p>
+                    <div className='mt-auto rounded-tl-lg rounded-tr-lg w-full text-[#225b6d]'>
+                        <ul className='flex gap-2 pt-2 flex-wrap'>
+                            <li className='flex gap-1 items-center border-r border-[#938885] pr-2'>
+                                <LikeIcon className="size-7" fill="#4597a8" /> {dish.opinions.filter(x => x.type == "like").length}
+                            </li>
+                            <li className='flex gap-1 items-center border-r border-[#938885] pr-2'>
+                                <DislikeIcon className="size-7" fill="#77b4d5" /> {dish.opinions.filter(x => x.type == "dislike").length}
+                            </li>
+                            <li className='flex gap-1 items-center border-r border-[#938885] pr-2'>
+                                <ClockIcon className="size-7" fill_face="#313d46" fill_hand="#313d46" /> {dish.steps.length} pasos
+                            </li>
+                            <li className='flex gap-1 items-center'>
+                                <CalendarIcon className="size-5"/>
+                                {new Date(dish.date.createdAt).toLocaleDateString()}
+                            </li>
+                        </ul>
                     </div>
+                    <span className='absolute right-5 top-4 md:bottom-5 border-lg px-4 bg-[#d3dfea] md:text-[17px] py-2 rounded-full text-center font-bold h-max text-[#337589] text-[12px]'>
+                        S/. {dish.price.toFixed(2)}
+                    </span>
                 </div>
             </div>
         </>
@@ -93,16 +92,19 @@ const SearchBar = () => {
 
     const [search, setSearch] = useState("");
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        setSearchParams({ search: search })
+    }
+
     return (
-        <div className='w-[85%] m-auto mt-[50px] flex'>
-            <input type="text" placeholder="Buscar" className='bg-[#f1f0f9] p-2 rounded-l-lg grow outline-none' onChange={(e) => setSearch(e.target.value)} />
-            <button className='cursor-pointer bg-[#b1b1b1] p-2' onClick={() => setSearchParams({
-                search: search
-            })}>
-                <SearchIcon className='size-6 inline mr-2' />
-                Buscar
+        <form className='w-full m-auto my-[15px] flex bg-[#eef1f6] rounded-[19px] border-[#93acb9] border-2 items-center pl-2' onSubmit={(e) => handleSearch(e)}>
+            <button className="hover:cursor-pointer outline-amber-300" onClick={(e) => handleSearch(e)}>
+                <SearchIcon className="size-[30px]" fill="#3e7892" />
             </button>
-        </div>
+            <input type="text" placeholder="Buscar" className='p-2 grow outline-none' onChange={(e) => setSearch(e.target.value)} />
+        </form>
     )
 }
 
@@ -110,11 +112,20 @@ const SearchBar = () => {
 * @param {{ dishes: import("../types/dish").Dish[] }} dishes
 */
 const DishesContainer = ({ dishes, page }) => {
+
+    const [searchParams] = useSearchParams();
+
+    const starred = dishes[0];
+
     return (
         <main className='w-[80%] mx-auto mt-[50px]'>
-            <StarredRecipe dish={dishes[0]} />
-            <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 bg-[#ecf4f2]'>
-                {dishes.slice(1, page * 10 + 1).map(dish => (
+            {!searchParams.get("search") && <StarredRecipe dish={starred} />}
+            <SearchBar />
+            <div className='grid grid-cols-1 gap-4'>
+                {searchParams.get("dishes") && dishes.slice(1, page * 10 + 1).map(dish => (
+                    <RecipeCard dish={dish} key={dish._id} />
+                ))}
+                {!searchParams.get("dishes") && dishes.map(dish => (
                     <RecipeCard dish={dish} key={dish._id} />
                 ))}
             </div>
@@ -166,14 +177,13 @@ function Panel() {
     return (
         <div id='login-page' className='pt-4'>
             <Header />
-            <SearchBar />
             {loading ? (
                 <div className='w-[80%] mx-auto mt-[50px]'>
                     <h1 className='text-center'>Cargando...</h1>
                 </div>
             ) : (
                 <>
-                    <DishesContainer dishes={filtered.sort((a, b) => ((a.likes - a.dislikes) > (b.likes - b.dislikes)) ? -1 : 1)} page={page} />
+                    <DishesContainer dishes={filtered.sort((a, b) => ((a.opinions.filter(x => x.type == "like") - (a.opinions.filter(x => x.type == "dislike"))) > ((b.opinions.filter(x => x.type == "like")) - (b.opinions.filter(x => x.type == "dislike")))) ? -1 : 1)} page={page} />
                     {page * 10 + 1 < dishes.length && <SeeMoreButton setPage={setPage} />}
                 </>
             )}
